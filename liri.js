a4c2d7d06fdb23ea46c488097ca1f13b36e55a08
MoChips2 <31714669+smedhan1@users.noreply.github.com>
2018-12-02 13:52:11 -0600

    final liri update

--- liri.js
+++ liri.js
@@ -11,28 +11,28 @@ var cmd = process.argv[2];
 
 // Liri Commands
 // ========================================================
+function generator() {
+    switch (cmd) {
+        case "concert-this":
+            bandsinTown();
+            break;
 
-switch (cmd) {
-    case "concert-this":
-        bandsinTown();
-        break;
+        case "spotify-this-song":
+            songChoice();
+            break;
 
-    case "spotify-this-song":
-        songChoice();
-        break;
+        case "movie-this":
+            movieSearch();
+            break;
 
-    case "movie-this":
-        movieSearch();
-        break;
+        case "do-what-it-says":
+            doWhat();
+            break;
 
-    case "do-what-it-says":
-        doWhat();
-        break;
-
-    default:
-        console.log("Please enter a valid argument!")
+        default:
+            console.log("Please enter a valid argument!")
+    }
 }
-
 
 // Bands in Town / concert-this
 // =========================================================
@@ -42,6 +42,9 @@ function bandsinTown() {
     var artist = process.argv.slice(3).join("%20");
     var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp?";
 
+    if(!artist) {
+        console.log("Artist not on tour. Please enter new artist.")
+    }
 
     axios.get(queryURL)
         .then(function (response, error) {
@@ -66,6 +69,7 @@ function bandsinTown() {
             else if (error) {
                 return console.log("No tour dates were found!")
             }
+
         })
 
 }
@@ -77,33 +81,34 @@ function songChoice(songTitle) {
 
     var spotify = new Spotify(keys.spotify);
     songTitle = process.argv.slice(3).join(" ");
-    var userChoice = process.argv.slice(3).join(" ");
-    
+
     if (!songTitle) {
-     songTitle = "The Sign";
+        songTitle = "The Sign";
+        // Could not log the hit classic "The Sign" by Ace of Base. This will log "The Sign" by Harry Styles instead. 
     }
-   
-    else if(songTitle === userChoice);
-        spotify.search({
-            type: 'track',
-            query: songTitle,
-            limit: 10
-        }).then(function (response) {
 
-            // console.log(JSON.stringify(response, null, 2));
 
-            var data = response.tracks;
+    spotify.search({
+        type: 'track',
+        query: songTitle,
+        limit: 10
+    }).then(function (response) {
 
-            var displaySong = "Artist: " + data.items[0].album.artists[0].name
-                              + "\nSong Title: " + data.items[0].name
-                              + "\nSong Preview: " + data.items[0].preview_url
-                              + "\nAlbum: " + data.items[0].album.name
-            console.log(displaySong);
-            
-        })
-            .catch(function (err) {
-                console.log(err);
-            });
+        // console.log(JSON.stringify(response, null, 2));
+
+        var data = response.tracks;
+
+        var displaySong = "Artist: " + data.items[0].album.artists[0].name
+            + "\nSong Title: " + data.items[0].name
+            + "\nSong Preview: " + data.items[0].preview_url
+            + "\nAlbum: " + data.items[0].album.name;
+        console.log(displaySong);
+
+    })
+        .catch(function (err) {
+            console.log(err);
+        });
+
 }
 
 
@@ -113,20 +118,32 @@ function songChoice(songTitle) {
 function movieSearch() {
 
     var movieName = process.argv.slice(3).join("%20");
+
+    if (!movieName) {
+        movieName = "Mr. Nobody";
+    }
+
     var queryURL = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
-    axios.get(queryURL).then(function (response) {
-        // console.log(response.data);
-        var data = response.data;
-        console.log("-------------------------------")
-        console.log("Title: " + data.Title + "\nYear: " + data.Year
-            + "\nIMDB Rating: " + data.Ratings[0].Value
-            + "\nRotten Tomatoes Rating: " + data.Ratings[1].Value
-            + "\nFilmed in: " + data.Country
-            + "\nLanguage: " + data.Language
-            + "\nPlot Summary: " + data.Plot
-            + "\nActors: " + data.Actors
-        )
+
+
+    axios.get(queryURL).then(function (response, error) {
+
+        if (response) {
+
+            var data = response.data;
+            console.log("-------------------------------")
+            console.log("Title: " + data.Title + "\nYear: " + data.Year
+                + "\nIMDB Rating: " + data.Ratings[0].Value
+                + "\nRotten Tomatoes Rating: " + data.Ratings[1].Value
+                + "\nFilmed in: " + data.Country
+                + "\nLanguage: " + data.Language
+                + "\nPlot Summary: " + data.Plot
+                + "\nActors: " + data.Actors
+            )
+        } else {
+            console.log(error);
+        }
     })
 
 
@@ -136,19 +153,24 @@ function movieSearch() {
 // Do what it says
 //==========================================================
 
-var rndmSong;
+
 
 function doWhat() {
 
-    fs.readFile("random.txt", "utf8", function (err, data) {
-        if (err) {
-            return console.log(err);
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
         }
         else {
             var textArray = data.split(",");
-                rndmSong = textArray[1];
-            songChoice(rndmSong);
+             songCMD = textArray[0];
+             rndmSong = textArray[1];
+             console.log(rndmSong);
+            /* instead of logging rndmSong, this is where I would somehow pass this value through songChoice function,
+             which would replace songTitle, search for "I Want It That Way" */
         }
     })
 
 }
+
+generator(cmd);
