36a9a60aa5aadd0fac42ace78087950f19a54aad
MoChips2 <31714669+smedhan1@users.noreply.github.com>
2018-12-01 23:00:39 -0600

    init commit

---
+++ liri.js
@@ -0,0 +1,126 @@
+// require('dotenv').config();
+
+var fs = require("fs");
+var keys = require("./keys.js");
+var axios = require("axios");
+var moment = require("moment");
+// var Spotify = require("node-spotify-api");
+
+
+var cmd = process.argv[2];
+
+// Liri Commands
+// ========================================================
+
+switch (cmd) {
+    case "concert-this":
+        bandsinTown();
+        break;
+
+    case "spotify-this-song":
+        Spotify();
+        break;
+
+    case "movie-this":
+        movieSearch();
+        break;
+
+    case "do-what-it-says":
+
+        break;
+
+    default:
+        console.log("Please enter a valid argument!")
+}
+
+
+// Bands in Town / concert-this
+// =========================================================
+
+function bandsinTown() {
+
+    var artist = process.argv.slice(3).join("%20");
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp?";
+
+
+    axios.get(queryURL)
+        .then(function (response, error) {
+
+
+            if (response) {
+                var dataArr = response.data;
+
+                //  console.log(dataArr);
+
+                for (i = 0; i < dataArr.length; i++) {
+
+                    var output = dataArr[i]
+
+                    //console.log(output)
+                    console.log("------------------------------")
+                    console.log(output.venue.name);
+                    console.log(output.venue.city + " " + output.venue.region);
+                    console.log(moment(output.datetime).format('MM/DD/YYYY'));
+                }
+            }
+            else if (error) {
+                return console.log("No tour dates were found!")
+            }
+        })
+
+}
+
+// Spotify Song
+//=========================================================
+
+function Spotify() {
+
+    var spotify = new Spotify(keys.spotify);
+    var songTitle = process.argv.slice(3).join(" ");
+
+    spotify.search({
+        type: 'track',
+        query: songTitle
+    }).then(function (response) {
+        console.log(response);
+    })
+        .catch(function (err) {
+            console.log(err);
+        });
+
+
+
+
+
+
+}
+
+
+//OMDB movie-this
+//==========================================================
+
+function movieSearch() {
+
+    var movieName = process.argv.slice(3).join("%20");
+    var queryURL = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+    axios.get(queryURL).then(function (response) {
+       // console.log(response.data);
+        var data = response.data;
+       console.log("-------------------------------")
+       console.log("Title: " + data.Title + "\nYear: " + data.Year
+                   + "\nIMDB Rating: " + data.Ratings[0].Value 
+                   + "\nRotten Tomatoes Rating: " + data.Ratings[1].Value
+                   + "\nCountry: " + data.Country
+                   + "\nLanguage: " + data.Language
+                   + "\nPlot Summary: " + data.Plot
+                   + "\nActors: " + data.Actors
+       )
+    })
+
+
+}
+
+
+// Do what it says
+//==========================================================
